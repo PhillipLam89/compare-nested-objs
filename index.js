bef6a950a53441b0660c1e49f0c921d82aa9460b
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2023-09-18 09:40:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const nanSTR = (v1,v2) => String(v1) == 'NaN' || String(v2) == 'NaN'
+const NaN_exists = (v1,v2) => String(v1) == 'NaN' || String(v2) == 'NaN'
+const number_exists = (n1,n2) => typeof n1 == 'number' || typeof n2 == 'number'
+let recursiveCount = 0
+
 function compareObjs(obj1, obj2) {
-  if (obj1 === obj2) return true
-  else if (obj1 == null || obj2 == null) return false
+  // console.log(`RECURSIVE PART ran ${recursiveCount++} times`)
+  const myStr = 'string function symbol'
+  if (obj1 === obj2 && obj1 !== 0) return true
+  if (obj1 == null || obj2 == null) return false
 
-  if (nanSTR(obj1,obj2)) {
-    return String(obj1) === String(obj2)
-  }else if (obj1.toFixed || obj2.toFixed) {
-    return obj1 === obj2
-  }
-  const specials = {function: 1,symbol:1,string:1}
 
-  if (typeof obj1 in specials||
-      typeof obj2 in specials) {
+  if (NaN_exists(obj1,obj2) || (obj1 === 0))
+    return obj1.toLocaleString() === obj2.toLocaleString()
+  else if (number_exists(obj1,obj2))
+    return obj1===obj2
+
+  if (myStr.includes(typeof obj1) || myStr.includes(typeof obj2)) {
         return String(obj1) === String(obj2)
     }
+    
   const keys1 = String(Object.keys(obj1))
   const keys2 = String(Object.keys(obj2))
   if (keys1 != keys2) return false
@@ -23,135 +27,3 @@ function compareObjs(obj1, obj2) {
   }
   return true
 }
-
-
-
-
-
-let obj1 = {
-  a: {phil: {age:30, job: ['teacher','blah']}},
-  b: 2,
-  c: { foo: 2, bar: 2},
-  d: { baz: 1, bat: 2, arr: [2,3] },
-  e: [1,2,{myNumber:3}],
-  f: function hi(x){},
-  g: null,
-  h: Symbol('his'),
-  i: NaN,
-  j: Infinity,
-  k: 'hi!',
-  l: Date.now(),
-  m: [Promise, String(55)]
-}
-
-let obj2 = {
-  a: {phil: {age:30, job: ['teacher','blah']}},
-  b: 2,
-  c: { foo: 2, bar: 2},
-  d: { baz: 1, bat: 2, arr: [2,3] },
-  e: [1,2,{myNumber:3}],
-  f: function hi(x){},
-  g: null,
-  h: Symbol('his'),
-  i: NaN,
-  j: Infinity,
-  k: 'hi!',
-  l: Date.now(),
-  m: [Promise, String(55)]
-}
-
-
-
-console.log(compareObjs(obj1,obj2)) //true
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function deepComparison (first, second) {
-//   if(first === second) return true
-//   if(first === null || second === null) return false
-//   const specials = ['symbol', 'function']
-
-//   if( specials.includes(typeof first)  || specials.includes(typeof first))  {
-//         return String(first) === String(second)
-//     }  else if (first.toFixed || second.toFixed) return first === second
-
-
-//   const first_keys = String(Object.getOwnPropertyNames(first))
-//   const second_keys = String(Object.getOwnPropertyNames(second))
-
-//   if(first_keys !== second_keys) return false
-
-//   for(const key of Object.getOwnPropertyNames(first)) {
-
-//       if (!deepComparison(first[key], second[key])) return false
-//   }
-//   return true
-// }
