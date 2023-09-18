ff728cc6afa950c22531298d24f2713d9de4d7c8
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2023-09-18 12:15:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function compare2Values(value1, value2) {
   console.log(`RECURSIVE PART ran ${recursiveCount++} times`)
   if (value1 === value2 && !bothAreZero(value1,value2))
       return true
-  if (value1 == null || value2 == null)
+  if (value1 === null || value2 === null)
       return false
 
   if (NaN_exists(value1,value2) || bothAreZero(value1,value2) )
@@ -18,16 +18,15 @@ function compare2Values(value1, value2) {
   else if (number_exists(value1,value2))
     return value1===value2
 
-  const myStr = 'string function symbol'
+  const myStr = 'function symbol'
   if (myStr.includes(typeof value1) || myStr.includes(typeof value2)) {
-        return String(value1)?.valueOf() ===
-                   String(value2)?.valueOf()
+        return String(value1)===String(value2)
     }
 
   const keys1 = String(Object.keys(value1))
   const keys2 = String(Object.keys(value2))
-  if (keys1 != keys2) return false
 
+  if (keys1 != keys2) return false
 
   for (const key of Object.keys(value1)) {
     if (!compare2Values(value1[key]?.valueOf(), value2[key]?.valueOf())) return false
@@ -69,10 +68,11 @@ const obj2 = {
   m: [Promise, String(Math.PI)]
 }
 
-console.log('2 nested arrays/objs =>',compare2Values(obj1,obj2)) //true
+console.log('2 same-value nested arrays/objs =>',compare2Values(obj1,obj2)) //true
 // console.log('array of same values =>',compare2Values( [1,2,3], [1,2,3] )) //true
 // console.log('array w/ DIFF values =>',compare2Values( [1,2,3], [999,2,3] )) //true
 // console.log('NaN === NaN =>',compare2Values(NaN, NaN)) //true
 // console.log('negative zero & zero =>',compare2Values(-0, +0)) //false
 // console.log('Symbols with same labels =>',compare2Values(Symbol('1'), Symbol('1'))) //true
 // console.log('Symbols with DIFF labels =>',compare2Values(Symbol('500'), Symbol('1'))) //false
+
