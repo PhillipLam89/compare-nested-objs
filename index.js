73c4dc8108426b9c662800959579a4a5ea7abb39
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2023-09-18 09:44:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let recursiveCount = 0
 
 function compareObjs(obj1, obj2) {
   // console.log(`RECURSIVE PART ran ${recursiveCount++} times`)
-  const myStr = 'string function symbol'
   if (obj1 === obj2 && obj1 !== 0) return true
   if (obj1 == null || obj2 == null) return false
 
@@ -14,6 +13,8 @@ function compareObjs(obj1, obj2) {
   else if (number_exists(obj1,obj2))
     return obj1===obj2
 
+
+  const myStr = 'string function symbol'
   if (myStr.includes(typeof obj1) || myStr.includes(typeof obj2)) {
         return String(obj1) === String(obj2)
     }
@@ -22,6 +23,7 @@ function compareObjs(obj1, obj2) {
   const keys2 = String(Object.keys(obj2))
   if (keys1 != keys2) return false
 
+  
   for (const key of Object.keys(obj1)) {
     if (!compareObjs(obj1[key], obj2[key])) return false
   }
