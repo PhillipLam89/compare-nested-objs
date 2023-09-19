4ffde32888c90b847cd7c035e914c00a0b01f857
Phillip Lam <64397693+PhillipLam89@users.noreply.github.com>
2023-09-18 20:43:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,26 +7,22 @@ const bothAreZero = (n1,n2) => n1 === 0 && n2 === 0
 let recursiveCount = 0
 
 function compare2Values(value1, value2) {
-  console.log(`RECURSIVE PART ran ${recursiveCount++} times`)
+  // console.log(`RECURSIVE PART ran ${recursiveCount++} times`)
+
   if (value1 === value2 && !bothAreZero(value1,value2))
       return true
 
-    // handles 'NaN' vs NaN
-  if (typeof value1 !== typeof value2) return false
-
-   // prevents type null == 'object' case
-  if (value1 === null || value2 === null)
-      return false
-
+  if (value1 == undefined || value2 == undefined) return false
           //handle #s NaN and neg/pos zeroes
-  if (NaN_exists(value1,value2) || bothAreZero(value1,value2) )
-    return value1.toLocaleString() === value2.toLocaleString()
-  else if (number_exists(value1,value2))
-    return value1===value2
+
+  const myStr = 'number string'
+  if (myStr.includes(typeof value1) || myStr.includes(typeof value2)) {
+         return Object.is(value1,value2)
+  }
 
         //handles func,symbol & string comparison
-  const myStr = 'function symbol string'
-  if (myStr.includes(typeof value1) || myStr.includes(typeof value2)) {
+  const myStr2 = 'function symbol'
+  if (myStr2.includes(typeof value1) || myStr2.includes(typeof value2)) {
         return String(value1)===String(value2)
     }
 
@@ -38,7 +34,7 @@ function compare2Values(value1, value2) {
   if (keys1 != keys2) return false
 
   for (const key of Object.keys(value1)) {
-    if (!compare2Values(value1[key]?.valueOf(), value2[key]?.valueOf())) return false
+    if (!compare2Values(value1[key], value2[key])) return false
   }
   return true
 }
@@ -48,9 +44,9 @@ function compare2Values(value1, value2) {
 const obj1 = {
   a: {phil: {age:30, job: ['teacher', class PHIL{}] }},
   b: 2,
-  c: { foo: new String('A'), x: {bar: 2,NaN:undefined} },
+  c: { foo: new String('Acc'), x: {bar: 2,NaN:null} },
   d: Array({undefined},1,2,[5]),
-  e: [1,2,{myNumber: new Number(3)}],
+  e: [1,2,{myNumber: Number(null)}],
   f: function hi(xs){return -0},
   g: null,
   h: Symbol(Object.prototype),
@@ -64,9 +60,9 @@ const obj1 = {
 const obj2 = {
   a: {phil: {age:30, job: ['teacher', class PHIL{}] }},
   b: 2,
-  c: { foo: new String('A'), x: {bar: 2,NaN:undefined} },
+  c: { foo: new String('Acc'), x: {bar: 2,NaN:null} },
   d: Array({undefined},1,2,[5]),
-  e: [1,2,{myNumber: new Number(3)}],
+  e: [1,2,{myNumber: Number(null)}],
   f: function hi(xs){return -0},
   g: null,
   h: Symbol(Object.prototype),
@@ -78,9 +74,10 @@ const obj2 = {
 }
 
 
+
 console.log('2 same-value nested arrays/objs =>',compare2Values(obj1,obj2)) //true
 // console.log('array of same values =>',compare2Values( [1,2,3], [1,2,3] )) //true
-// console.log('array w/ DIFF values =>',compare2Values( [1,2,3], [999,2,3] )) //false
+// console.log('array w/ DIFF values =>',compare2Values( [1,2,3], [999,2,3] )) //true
 // console.log('NaN === NaN =>',compare2Values(NaN, NaN)) //true
 // console.log('negative zero & zero =>',compare2Values(-0, +0)) //false
 // console.log('Symbols with same labels =>',compare2Values(Symbol('1'), Symbol('1'))) //true
